5e25b636080fdf2068b22f8b6f75beeff5258c9c
Erik Bosman <ebosman@gmail.com>
2004-10-14 22:02:56 +0000

    sysv style timeouts

--- compat.h
+++ compat.h
@@ -7,5 +7,9 @@ typedef u_int8_t u8_t;
 typedef u_int16_t u16_t;
 typedef u_int32_t u32_t;
 
+typedef void (*sighandler_t)(int);
+sighandler_t sysv_signal(int signum, sighandler_t handler);
+#define signal sysv_signal
+
 #endif
 
