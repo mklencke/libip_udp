9bbc000a0824ad67ca1924c7a21b829cbe372945
Marten Klencke <mklencke@gmail.com>
2004-10-17 12:47:20 +0000

    License frenzy

--- ip.c
+++ ip.c
@@ -1,18 +1,21 @@
 /*
- * Computer Networks Practicum IP Layer Emulation
+ * libip_udp -- Computer Networks Practicum IP Layer Emulation
  *
- * This library emulates the CN IP Layer by sending fake IP packets
- * through UDP. This eliminates Minix during development time and thus
- * makes the whole process much easier.
+ * Copyright (C) 2004 Marten Klencke & Erik Bosman
  *
- * It is being developed on Slackware Linux 10.0.
- *
- * Copyright (C) 2004:
- * Marten Klencke (mklencke@cs.vu.nl)
- * Erik Bosman (ejbosman@cs.vu.nl)
- * 
- * This code is licensed under the GNU LGPL
- *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #include <sys/socket.h>
